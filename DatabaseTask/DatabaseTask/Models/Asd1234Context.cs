a41be2bac7d9b2f61416498dc5ec0fd26682c14d
Opilane TTHK <opilane@tthk.ee>
2025-09-29 11:19:52 +0300

    Database-First Migration

---
+++ DatabaseTask/DatabaseTask/Models/Asd1234Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseTask.Models;
+
+public partial class Asd1234Context : DbContext
+{
+    public Asd1234Context()
+    {
+    }
+
+    public Asd1234Context(DbContextOptions<Asd1234Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=Asd1234;TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
