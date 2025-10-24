8bebf50c7d81b1c923ad5f4ba89bd30e0d5e876e
Opilane TTHK <opilane@tthk.ee>
2025-10-24 14:58:59 +0300

    Project fix

---
+++ DatabaseTask/DatabaseTask.Data/DatabaseTaskBbContext.cs
@@ -0,0 +1,13 @@
+﻿using DatabaseTask.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseTask.Data
+{
+    public class DatabaseTaskBbContext : DbContext
+    {
+        public DatabaseTaskBbContext(DbContextOptions<DatabaseTaskBbContext> options) 
+        : base(options) { }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
