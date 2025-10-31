f8a010dd9d41b88effee74538c9d8ed15f7bb573
Opilane TTHK <ArturVartsaba1@gmail.com>
2025-10-31 14:12:54 +0200

    Clean Project

--- DatabaseTask/DatabaseTask.Data/DatabaseTaskBbContext.cs
+++ DatabaseTask/DatabaseTask.Data/DatabaseTaskBbContext.cs
@@ -8,13 +8,6 @@ namespace DatabaseTask.Data
         public DatabaseTaskBbContext(DbContextOptions<DatabaseTaskBbContext> options) 
         : base(options) { }
 
-        public DbSet<Child> Child { get; set; }
-        public DbSet<ChildAbsence> ChildAbsence { get; set; }
-        public DbSet<ChildGroupHistory> ChildGroupHistory { get; set; }
-        public DbSet<Employee> Employee { get; set; }
-        public DbSet<Group> Group { get; set; }
-        public DbSet<Menu> Menu { get; set; }
-        public DbSet<Position> Position { get; set; }
-        public DbSet<Queue> Queue { get; set; }
+        
     }
 }
